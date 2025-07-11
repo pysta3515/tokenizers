41d6f3f725f9dc3b1429985e776a46ea2ac39a94
pysta3515 <245539763@qq.com>
2025-07-11 02:15:52 +0000

    add encode_batch_debug.log

--- tokenizers/examples/encode_batch.rs
+++ tokenizers/examples/encode_batch.rs
@@ -4,7 +4,7 @@ use tokenizers::{Tokenizer};
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let tokenizer = Tokenizer::from_file("/workspaces/tokenizers/tokenizers/examples/tokenizer.json")?;
 
-    let encoding = tokenizer.encode("hello, world [PAD]", false)?;
+    let encoding = tokenizer.encode("hello, 🤗 [PAD] [UNK]", false)?;
     println!("out:{:?}", encoding.get_tokens());
     Ok(())
 }
